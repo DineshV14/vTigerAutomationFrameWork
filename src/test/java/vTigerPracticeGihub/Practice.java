688650c31a8969bd7045dc5c38f69ec18949f79a
HP <HP@DESKTOP-F9V4Q9L>
2022-12-09 03:51:52 +0530

    Commited by Ankush V. printSomethingElse()

--- src/test/java/vTigerPracticeGihub/Practice.java
+++ src/test/java/vTigerPracticeGihub/Practice.java
@@ -5,10 +5,16 @@ import org.testng.annotations.Test;
 
 public class Practice {
 
-	@Test
+	@Test(priority = 1)
 	public void printSomething()
 	{
 		System.out.println("Jay Bhim");
 		Reporter.log("JayBhim");
+	}
+	
+	@Test(priority = 2)
+	public void printSomeThingElse()
+	{
+		System.out.println("Jor Se Bolo Jai Mata Di");
 	}
 }
