746314016c0d2748fa74ab61017447fe0d144bdf
HP <HP@DESKTOP-F9V4Q9L>
2022-12-09 02:59:46 +0530

    commit and push by user 1

---
+++ src/test/java/vTigerPracticeGihub/Practice.java
@@ -0,0 +1,14 @@
+package vTigerPracticeGihub;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Practice {
+
+	@Test
+	public void printSomething()
+	{
+		System.out.println("Jay Bhim");
+		Reporter.log("JayBhim");
+	}
+}
